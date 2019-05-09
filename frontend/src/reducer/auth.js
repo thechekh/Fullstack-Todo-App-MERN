caebe837e0406ff369eacb6938fbf32484bcd284
DanilChekh <chexdanil@gmail.com>
2019-05-09 11:34:18 +0300

    need rework logout,other work

--- frontend/src/reducer/auth.js
+++ frontend/src/reducer/auth.js
@@ -1,18 +1,16 @@
 import { userConstants } from '../constants';
 
 let user = JSON.parse(localStorage.getItem('user'));
-const initialState = user ? { loggedIn: true, user } : {};
+const initialState = user ? { user } : {};
 
 export default(state = initialState, action) =>{
   switch (action.type) {
     case userConstants.LOGIN_REQUEST:
       return {
-        loggingIn: true,
         user: action.user
       };
     case userConstants.LOGIN_SUCCESS:
       return {
-        loggedIn: true,
         user: action.user
       };
     case userConstants.LOGIN_FAILURE:
