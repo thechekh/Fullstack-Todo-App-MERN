16400461b4ae08eaa032275dd3047f12647c1390
DanilChekh <chexdanil@gmail.com>
2019-05-09 12:06:02 +0300

    add redux compose

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,15 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware,compose} from 'redux'
 import reducer from '../reducer/index'
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 
 const loggerMiddleware = createLogger();
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
 const store = createStore(
     reducer,
     composeEnhancers(
